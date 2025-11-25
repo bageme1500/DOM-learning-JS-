fbb540ea17a41365cafb93f69d4a94854cdecb15
Boniphace <boniphacebageme06@gmail.com>
2025-11-25 10:31:44 +0300

    implemented the button for calling the function game()

--- index.js
+++ index.js
@@ -1,21 +1,24 @@
-let num = Math.random();
-a = Math.round(num * 5) + 1;
-name = 'dice' + a;
-img1 = name + '.'+'png';
 
-let num2 = Math.random();
-b = Math.round(num2 * 5) + 1;
-name2 = 'dice' + b;
-img2 = name2 + '.'+'png';
+function game(){
+  let num = Math.random();
+  a = Math.round(num * 5) + 1;
+  name = 'dice' + a;
+  img1 = name + '.'+'png';
+  
+  let num2 = Math.random();
+  b = Math.round(num2 * 5) + 1;
+  name2 = 'dice' + b;
+  img2 = name2 + '.'+'png';
 
 
-document.querySelectorAll("img")[0].setAttribute('src', 'images/'+ img1);
-document.querySelectorAll("img")[1].setAttribute('src', 'images/' + img2);
+  document.querySelectorAll("img")[0].setAttribute('src', 'images/'+ img1);
+  document.querySelectorAll("img")[1].setAttribute('src', 'images/' + img2);
 
-if(a > b){
-  document.querySelector('h1').innerHTML ='🚩Player one Won!';
-}else if(b > a){
-  document.querySelector('h1').innerHTML = 'Player two won🚩';
-}else{
-  document.querySelector('h1').innerHTML = "Draw try again 👌";
+  if(a > b){
+    document.querySelector('h1').innerHTML ='🚩Player one Won!';
+  }else if(b > a){
+    document.querySelector('h1').innerHTML = 'Player two won🚩';
+  }else{
+    document.querySelector('h1').innerHTML = "Draw try again 👌";
+  }
 }
